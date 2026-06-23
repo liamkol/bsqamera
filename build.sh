d7538e15006cc9bf291409ff24cb8e56bb3dfc38
liamkol <liamkolman10@gmail.com>
2026-06-23 16:54:49 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$ANDROID_NDK_HOME" ]; then
-    echo "ANDROID_NDK_HOME is not set. Point it at NDK r27+." >&2
-    exit 1
-fi
-
 mkdir -p build
 cd build
 
 cmake -G Ninja \
-    -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-    -DANDROID_ABI=arm64-v8a \
-    -DANDROID_PLATFORM=android-24 \
     -DCMAKE_BUILD_TYPE=Release \
     ..
 
